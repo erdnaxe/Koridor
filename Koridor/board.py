c07b7a22c8758b357b3a379bd103b13b0ea3ab13
erdnaxe <alexandre.iooss@gmail.com>
2015-08-03 21:01:54 +0200

    Add board class.

--- Koridor/board.py
+++ Koridor/board.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+from case import *
+
+
+class Board:
+    def __init__(self):
+        self.resetBoard()
+
+    # Create a 2D array contenant des objets "case"
+    def resetBoard(self):
+        self.map = []
+        for i in range(10):
+            self.map.append([Case()] * 10)
