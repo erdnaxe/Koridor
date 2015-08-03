e0115c16f0e47c97b135dab8d39d1ebcee98079e
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-08-03 21:19:49 +0200

    creation du squelette de Game

--- Koridor/game.py
+++ Koridor/game.py
@@ -5,3 +5,15 @@ from board import *
 class Game:
 
     def __init__(self):
+        # definition du plateau
+
+        # definition des players
+
+    def newGame(self)):
+        # initialisation de tout
+
+    def play(self, action):
+        #return 1 si mouvement effectue, 0 sinon
+
+    def verifiedAction(self, action):
+        # return 1 si action possible, 0 sinon
