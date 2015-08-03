184e873a8c69bc1fe44a571e550a60ad9c3edc2a
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-08-03 21:05:11 +0200

    definition de la classe case

--- Koridor/case.py
+++ Koridor/case.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+
+class Case:
+    idClass = 1
+
+    def __init__(self):
+        self.id = Case.idClass
+        Case.idClass += 1
+
+        self.walls = 4 * [0]  # 1 : haut, 2 : droite, 3 : bas, 4 : gauche
+        self.player = None
+
