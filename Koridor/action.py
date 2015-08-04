e9f158c2b3a78a5fdfe2e2051ca967bb3946ceb9
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-08-04 11:36:37 +0200

    plus d'action

--- Koridor/action.py
+++ Koridor/action.py
@@ -5,18 +5,28 @@ class Action:
     """
         This class create an object to encode the player wish of action
     """
+    allActions = ["go_forward", "go_backward", "go_left",
+                "go_right", "place_wall"]
+
     def __init__(self):
         """
             constructor
         """
         self.action = ""
-        self.wallCoordinate = [0,0]
+        self.wallCoordinate = [0, 0]
         self.wallType = 0
         # 1 : le mur commence en haut et s'etend a gauche
-        # 2 : le mur commence a droite et
+        # 2 : le mur commence a droite et s'etend en bas
+        # 3 : le mur commence en bas et s'etend a gauche
+        # 4 : le mur commence a geuche et s'etend en bas'
 
     def stringToAction(self, actionSring):
         """
             This method take a string and init the Action object depending of it
         """
-
+        if actionSring in Action.allActions[:4]:
+            self.action = actionSring
+            return True
+        elif actionSring in Action.allActions[4]:
+            # cas du placement de mur
+            return True
