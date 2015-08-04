06d3eed2f31cf213cb0c9ec300e42ad743e08dba
erdnaxe <alexandre.iooss@gmail.com>
2015-08-04 10:46:36 +0200

    The class to draw the grid is finished !

--- Koridor/shellInterface.py
+++ Koridor/shellInterface.py
@@ -13,23 +13,47 @@ class ShellInterface:
         self.map = game.board.map
 
     def drawGrid(self):
+        """
+            Method to draw the grid
+        """
         for x in range(self.size):
             for y in range(self.size):
                 self.drawMiddleCase(x, y)
                 if y != self.size - 1:
-                    self.drawVerticalWall(x, y, 2)
+                    self.drawVerticalWall(x, y)
                 else:
                     print()  # new line
-                    if x != self.size - 1:
-                        print('--+--+--+--+--+--+--+--+--')
+                    self.drawHorizontalWalls(x)
 
-    def drawVerticalWall(self, x, y, side):
-        if self.map[x][y].hasWall(side):
+    def drawVerticalWall(self, x, y):
+        """
+            Method to draw the wall between 2 players
+        """
+        if self.map[x][y].hasWall(2):
             print('█', end='')
         else:
             print('|', end='')
 
+    def drawHorizontalWalls(self, x):
+        """
+            Method to draw a horizontal line of walls (not the player line)
+        """
+        if x != self.size - 1:
+            for y in range(self.size):
+                if self.map[x][y].hasWall(1):
+                    print('■■', end='')
+                else:
+                    print('--', end='')
+
+                # Draw the plus
+                if y != self.size - 1:
+                    print('+', end='')
+            print()  # new line
+
     def drawMiddleCase(self, x, y):
+        """
+            Method to draw the center of a case (with the player if he is here)
+        """
         if self.map[x][y].player is None:
             print('  ', end='')
         else:
