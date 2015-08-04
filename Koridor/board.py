8d2452acf0bf8fbf92d87b83e0f2dd66c8f0c398
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-08-04 12:29:13 +0200

    modif de board

--- Koridor/board.py
+++ Koridor/board.py
@@ -8,21 +8,35 @@ from case import *
 
 
 class Board:
+    """
+    this method implement the board of the game
+    """
     def __init__(self, players):
+        """
+        constructor
+        """
         self.size = 9
         self.map = []
         self.players = players
         for i in range(self.size):
             self.map.append([Case()] * self.size)
 
-    # Create a 2D array contenant des objets "case"
     def resetBoard(self):
+        """
+            Create a 2D array contenant des objets "case"
+        """
         self.__init__()
 
     def getCase(self, x, y):
+        """
+            retourne la case correspondant a la position
+        """
         return self.map[x][y]
 
-    def getPlayerByCase(self, position):
+    def getPlayerByPosition(self, position):
+        """
+            return le joueur correspondant a la position
+        """
         for player in self.players:
             if player.position == position:
                 return player.id
