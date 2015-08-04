3b0541df869885bbd231268877ff1ca75f45aa59
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-08-04 10:59:08 +0200

    plus d'action

--- Koridor/action.py
+++ Koridor/action.py
@@ -9,3 +9,14 @@ class Action:
         """
             constructor
         """
+        self.action = ""
+        self.wallCoordinate = [0,0]
+        self.wallType = 0
+        # 1 : le mur commence en haut et s'etend a gauche
+        # 2 : le mur commence a droite et
+
+    def stringToAction(self, actionSring):
+        """
+            This method take a string and init the Action object depending of it
+        """
+
