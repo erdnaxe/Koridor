fe042475707abfa6f8691cd70637806954663ec2
erdnaxe <alexandre.iooss@gmail.com>
2015-08-05 11:29:05 +0200

    Clean the way of drawing horizontal lines.

--- Koridor/shellInterface.py
+++ Koridor/shellInterface.py
@@ -7,6 +7,10 @@ you can draw a Koridor game in a monospace Shell.
 
 
 class ShellInterface:
+    """
+        With the ShellInterface you can draw a Koridor game in a
+        monospace Shell. It uses ascii box characters.
+    """
 
     def __init__(self, game):
         self.board = game.board
@@ -21,9 +25,13 @@ class ShellInterface:
         for x in range(self.board.size):
             print((self.horizontalGridLine(x)))
 
+            if x != self.board.size - 1:
+                # draw the bottom horizontal line of walls
+                print((self.horizontalWalls(self.board.map[x])))
+
     def horizontalGridLine(self, x):
         """
-            Method to return 2 lines of the grid (player line and walls line)
+            Method to return a horizontal line of vertical walls and players
         """
         tmpReturn = ''
 
@@ -32,14 +40,18 @@ class ShellInterface:
             if y != self.board.size - 1:
                 # draw right wall
                 tmpReturn += self.verticalWall(self.board.map[x][y])
-            else:
-                tmpReturn += '\n'
-
-                if x != self.board.size - 1:
-                    # draw the bottom horizontal line of walls
-                    tmpReturn += self.horizontalWalls(self.board.map[x])
 
         return tmpReturn
+
+    def centerCase(self, position):
+        """
+            Method to return the center of a case
+        """
+        playerId = self.board.getPlayerByPosition(position)
+        if playerId != 0:
+            return str(playerId) + ' '
+        else:
+            return '  '
 
     def verticalWall(self, case):
         """
@@ -68,16 +80,7 @@ class ShellInterface:
 
         return tmpReturn
 
-    def centerCase(self, position):
-        """
-            Method to return the center of a case
-        """
-        playerId = self.board.getPlayerByPosition(position)
-        if playerId != 0:
-            return str(playerId) + ' '
-        else:
-            return '  '
-
     def askAction(self):
-        print("C'est au joueur " + str(self.activePlayer + 1) + " de jouer.")
-        return input("> ")
+        print(("C'est au joueur " + str(self.activePlayer + 1) + " de jouer."))
+        command = str(input("> "))
+        return command
