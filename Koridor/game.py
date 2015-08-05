6dbad1a1b79197fcf5c9f79f85f0d2caba967536
erdnaxe <alexandre.iooss@gmail.com>
2015-08-05 11:14:00 +0200

    Fix isFinished

--- Koridor/game.py
+++ Koridor/game.py
@@ -89,6 +89,8 @@ class Game:
 
     def isFinished(self):
         for player in self.players:
-            if (player.initposition[0] - player.position[0]) ** 2 == 81:
+            if (player.initPosition[0] - player.position[0]) == 9:
+                return player.id
+            if (player.position[0] - player.initPosition[0]) == 9:
                 return player.id
         return 0
