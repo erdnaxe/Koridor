84277f5e37b2c495b5ddd42fe0b0a2954dcf8e10
erdnaxe <alexandre.iooss@gmail.com>
2015-08-16 11:37:32 +0200

    Better walls

--- Koridor/interface/lines.py
+++ Koridor/interface/lines.py
@@ -7,7 +7,7 @@ class Lines():
         Line between (x1, y1) and (x2, y2)
     """
 
-    def __init__(self, x1, y1, x2, y2, origin=[0, 0], color=[255, 255, 255]):
+    def __init__(self, x1, y1, x2, y2, origin, color=[255, 255, 255], width=1):
         """
             Constructor
         """
@@ -18,9 +18,11 @@ class Lines():
                     ('c3B', (color[0], color[1], color[2],
                             color[0], color[1], color[2]))
         )
+        self.width = width
 
     def draw(self):
         """
             Draw the object
         """
+        pyglet.graphics.glLineWidth(self.width)
         self.lines.draw(pyglet.gl.GL_LINES)
--- Koridor/interface/wall.py
+++ Koridor/interface/wall.py
@@ -10,6 +10,7 @@ class Wall:
             Constructor
         """
         color = [255, 0, 0]
+        width = 3
 
         # Origin coords of the case
         x = coord[0] * grid.width // grid.size_x
@@ -18,17 +19,17 @@ class Wall:
         if side == 1:
             x2 = x + grid.width // grid.size_x
             y1 = y + grid.width // grid.size_y
-            self.line = Lines(x, y1, x2, y1, grid.origin, color)
+            self.line = Lines(x, y1, x2, y1, grid.origin, color, width)
         elif side == 2:
             x1 = x + grid.width // grid.size_x
             y2 = y + grid.width // grid.size_y
-            self.line = Lines(x1, y, x1, y2, grid.origin, color)
+            self.line = Lines(x1, y, x1, y2, grid.origin, color, width)
         elif side == 3:
             x2 = x + grid.width // grid.size_x
-            self.line = Lines(x, y, x2, y, grid.origin, color)
+            self.line = Lines(x, y, x2, y, grid.origin, color, width)
         else:
             y2 = y + grid.width // grid.size_y
-            self.line = Lines(x, y, x, y2, grid.origin, color)
+            self.line = Lines(x, y, x, y2, grid.origin, color, width)
 
     def draw(self):
         """
